469965bdd41b81f1043e66876b1329125c64ca7a
Harsh Rawat <harsh.vorphy@gmail.com>
2022-11-02 00:26:00 +0530

    ADD: task solutions.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [visibilty, setVisibilty] = useState("password");
+
+  useEffect(() => {
+    if (password.length < 4 && password != "") {
+      setError("Password must be greater than 4 character");
+      return;
+    }
+    setError("");
+  }, [password]);
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+
+  const handlePasswordVisibility = () => {
+    if (visibilty === "text") {
+      setVisibilty("password");
+    } else {
+      setVisibilty("text");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-card">
+        <div className="card-body">
+          <form className="form">
+            <input
+              type={visibilty}
+              name="password"
+              value={password}
+              onChange={({ target: { value } }) => {
+                handlePasswordChange(value);
+              }}
+            />
+            <button type={"button"} onClick={handlePasswordVisibility}>
+              Toggle Password Visibilty
+            </button>
+            <span>{error}</span>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
